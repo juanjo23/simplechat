2e01ebca1b2a3eb8dd9acb1bd85474c66c0e09b1
juanjo23 <juanjo_23@live.com>
2014-08-30 00:21:26 -0500

    Some changes

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,13 +1,36 @@
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 
 public class MainWindow extends JFrame {
 	public MainWindow(){
-		addComponentTo(null, null, 0, 0, 0, 0, 0, 0, 0);
+		
+		this.setSize(400, 460);
+		this.setTitle("Simple chat");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+		JPanel contenedor = new JPanel(); 
+		contenedor.setLayout(new GridBagLayout());
+		
+		JTextArea area = new JTextArea();
+		area.setBackground(new Color(30, 30, 30));
+		area.setForeground(Color.WHITE);
+		
+		addComponentTo(contenedor, area,   0, 0, 2, 1, 1.0, 1.0, GridBagConstraints.BOTH);
+		addComponentTo(contenedor, new JTextField(),   0, 1, 1, 1, 1.0, 0.0, GridBagConstraints.HORIZONTAL);
+		addComponentTo(contenedor, new JButton("Send"),1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.HORIZONTAL);
+		this.getContentPane().add(contenedor);
+		this.setVisible(true);
+					
 	}
 	private void addComponentTo(JPanel panel, Component comp, int posX, int posY,
 			int width, int height, double weightX, double weightY, int fill){
