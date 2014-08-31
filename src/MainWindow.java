736ed9102caec5aa1adb65fdf69cf59706430dc5
juanjo23 <juanjo_23@live.com>
2014-08-31 14:49:30 -0500

    Cambios en la GUI

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,37 +1,77 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JToolBar;
 
 
-public class MainWindow extends JFrame {
+public class MainWindow extends JFrame implements ActionListener {
+	private JTextArea areaChat;
+	private JTextField  txtSend;
+	private JButton btnSend;
+	private JButton btnClient;
+	private JButton btnServer;
+	
 	public MainWindow(){
 		
 		this.setSize(400, 460);
 		this.setTitle("Simple chat");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
-		JPanel contenedor = new JPanel(); 
-		contenedor.setLayout(new GridBagLayout());
-		
-		JTextArea area = new JTextArea();
-		area.setBackground(new Color(30, 30, 30));
-		area.setForeground(Color.WHITE);
-		
-		addComponentTo(contenedor, area,   0, 0, 2, 1, 1.0, 1.0, GridBagConstraints.BOTH);
-		addComponentTo(contenedor, new JTextField(),   0, 1, 1, 1, 1.0, 0.0, GridBagConstraints.HORIZONTAL);
-		addComponentTo(contenedor, new JButton("Send"),1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.HORIZONTAL);
-		this.getContentPane().add(contenedor);
+		setComponents();	
+		this.setLocationRelativeTo(null);
 		this.setVisible(true);
+		
+		txtSend.requestFocusInWindow();
+		btnClient.addActionListener(this);
+		btnServer.addActionListener(this);
+		btnSend.addActionListener(this);
 					
 	}
+	
+	private void setComponents(){			
+		JPanel contenedor = new JPanel(); 		
+		contenedor.setLayout(new GridBagLayout());
+		
+		areaChat = new JTextArea();
+		areaChat.setForeground(Color.WHITE);
+		areaChat.setBackground(new Color(40, 40, 40));
+		
+		txtSend = new JTextField();
+		txtSend.setBackground(new Color(215, 220, 218));
+		
+		btnSend = new JButton("Send");
+		btnSend.setBackground(new Color(51, 181, 229));
+		btnSend.setForeground(Color.WHITE);
+
+
+		addComponentTo(contenedor, areaChat,0, 0, 2, 1, 1.0, 1.0, GridBagConstraints.BOTH);
+		addComponentTo(contenedor, txtSend, 0, 1, 1, 1, 1.0, 0.0, GridBagConstraints.BOTH);
+		addComponentTo(contenedor, btnSend, 1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.BOTH);
+		this.getContentPane().add(contenedor);
+		
+		JToolBar mBar = new JToolBar();
+		mBar.setFloatable(false);
+		ImageIcon imgClient = new ImageIcon(this.getClass().getResource("images/client.png"));
+		ImageIcon imgServer = new ImageIcon(this.getClass().getResource("images/server.png"));
+		btnClient = new JButton("Client", imgClient);
+		btnServer = new JButton("Server", imgServer);
+		
+		mBar.add(btnServer);
+		mBar.add(btnClient);
+		this.getContentPane().add(mBar, BorderLayout.NORTH);		
+		
+	}
+	
 	private void addComponentTo(JPanel panel, Component comp, int posX, int posY,
 			int width, int height, double weightX, double weightY, int fill){
 		
@@ -44,5 +84,13 @@ public class MainWindow extends JFrame {
 		gbc.weighty = weightY;
 		gbc.fill = fill;
 		panel.add(comp, gbc);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == btnServer){
+			areaChat.append("Servidor creado con éxito");
+		}
+		
 	}	
 }
--- src/Server.java
+++ src/Server.java
@@ -1,26 +1,56 @@
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.swing.JTextArea;
 
-public class Server {
-	private ServerSocket ss; 
-	private Socket s;
-	private ObjectOutputStream oos;
-	
-	public Server(){
+
+public class Server implements Runnable {
+	private ServerSocket serverSocket; 
+	private Socket socket;
+	private ObjectOutputStream oOut;
+	private ObjectInputStream oIn;
+	private JTextArea areachat; 
+	public Server(JTextArea areachat){		
+		this.areachat = areachat;
+	}
+
+	@Override
+	public void run() {
 		try{
-			ss = new ServerSocket(9999);
-			s = ss.accept();
-			oos = new ObjectOutputStream(s.getOutputStream());
-			oos.writeObject("Hola cliente feo");
-			
-			oos.close();
-			s.close();
-			ss.close();
+			serverSocket = new ServerSocket(9999);
+			socket = serverSocket.accept();
+			oOut = new ObjectOutputStream(socket.getOutputStream());
+			oIn  = new ObjectInputStream(socket.getInputStream());			
 			
 		}catch(Exception e){
 			
-		}		
-	 }
+		}
+	}
+	public void writeLine(String line){
+		try{
+			oOut.writeObject(line);
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+	public void readLine(){
+		try{
+			while(true){
+				String line = oIn.readObject().toString();				
+				areachat.append("Client: "+line);
+				Thread.sleep(15);				
+			}
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+	public void closeServer(){
+		try{
+			socket.close();
+			serverSocket.close();
+		}catch(Exception e){ }
+	}
 }
