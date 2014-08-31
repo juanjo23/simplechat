b504835df9dc82a61e1c35d9ddb85c823810564f
Juanjo <juanjo_23@live.com>
2014-08-31 02:47:16 -0500

    s

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
@@ -16,22 +17,35 @@ public class MainWindow extends JFrame {
 		this.setSize(400, 460);
 		this.setTitle("Simple chat");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
+		setComponents();	
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+					
+	}
+	
+	private void setComponents(){
 		JPanel contenedor = new JPanel(); 
 		contenedor.setLayout(new GridBagLayout());
 		
 		JTextArea area = new JTextArea();
-		area.setBackground(new Color(30, 30, 30));
 		area.setForeground(Color.WHITE);
+		area.setBackground(new Color(40, 40, 40));
 		
+		JTextField txtSend = new JTextField();
+		txtSend.setBackground(new Color(215, 220, 218));
+		//txtSend.setPreferredSize(new Dimension(300, 30));
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.setBackground(new Color(51, 181, 229));
+		btnSend.setForeground(Color.WHITE);
+
+
 		addComponentTo(contenedor, area,   0, 0, 2, 1, 1.0, 1.0, GridBagConstraints.BOTH);
-		addComponentTo(contenedor, new JTextField(),   0, 1, 1, 1, 1.0, 0.0, GridBagConstraints.HORIZONTAL);
-		addComponentTo(contenedor, new JButton("Send"),1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.HORIZONTAL);
-		this.getContentPane().add(contenedor);
-		this.setVisible(true);
-					
+		addComponentTo(contenedor, txtSend, 0, 1, 1, 1, 1.0, 0.0, GridBagConstraints.BOTH);
+		addComponentTo(contenedor, btnSend, 1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.BOTH);
+		this.getContentPane().add(contenedor);		
 	}
+	
 	private void addComponentTo(JPanel panel, Component comp, int posX, int posY,
 			int width, int height, double weightX, double weightY, int fill){
 		
